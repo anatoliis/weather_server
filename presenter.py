6a2b24f539a22e422063707ac9cd1c5500352904
anatoliis <anatolii@fastmail.com>
2017-08-06 16:08:03 +0300

    Adds output formatting method to measurement presenter

--- presenter.py
+++ presenter.py
@@ -5,45 +5,47 @@ class MeasurementPresenter:
     def __init__(self, measurement):
         self._measurement = measurement
 
+    def _formatted(self, value):
+        return '{:.2f}'.format(value)
+
     @property
     def temperature_1(self):
-        return '{:.2f}'.format(self._measurement['t1'])
+        return self._formatted(self._measurement['t1'])
 
     @property
     def temperature_2(self):
-        return '{:.2f}'.format(self._measurement['t2'])
+        return self._formatted(self._measurement['t2'])
 
     @property
     def temperature_3(self):
-        return '{:.2f}'.format(self._measurement['t3'])
+        return self._formatted(self._measurement['t3'])
 
     @property
     def avg_temperature(self):
         avg = (self._measurement['t1'] + self._measurement['t2'] + self._measurement['t3']) / 3.
-        return '{:.2f}'.format(avg)
+        return self._formatted(avg)
 
     @property
     def temperature_collector(self):
-        return '{:.2f}'.format(self._measurement['tc'])
+        return self._formatted(self._measurement['tc'])
 
     @property
     def temperature_unit(self):
-        return '{:.2f}'.format(self._measurement['t0'])
+        return self._formatted(self._measurement['t0'])
 
     @property
     def pressure_pa(self):
-        return '{:.2f}'.format(self._measurement['pr'])
+        return self._formatted(self._measurement['pr'])
 
     @property
     def pressure_mm(self):
-        return '{:.2f}'.format(self._measurement['pr'] / 133.3223684)
+        return self._formatted(self._measurement['pr'] / 133.3223684)
 
     @property
     def humidity(self):
-        return '{:.2f}'.format(self._measurement['hm'])
+        return self._formatted(self._measurement['hm'])
 
     @property
     def timestamp(self):
         date = datetime.fromtimestamp(self._measurement.original_timestamp)
-
         return date.strftime('%H:%M:%S')
